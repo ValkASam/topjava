946a16efb8511c6603f0011c5d367750f48d495c
ValkSam <do110473sva@gmail.com>
2015-09-29 16:46:00 +0300

    HW3 : all patches (3_01_* - 3_11_*) applied

---
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -0,0 +1,28 @@
+package ru.javawebinar.topjava.service;
+
+
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.util.Collection;
+
+/**
+ * User: gkislin
+ * Date: 22.08.2014
+ */
+public interface UserService {
+
+    public User save(User user);
+
+    public void delete(int id) throws NotFoundException;
+
+    public User get(int id) throws NotFoundException;
+
+    Collection<User> getAll();
+
+    public User getByEmail(String email) throws NotFoundException;
+
+    void update(User user) throws NotFoundException;
+    
+
+}
