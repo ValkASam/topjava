461f90f8b773cc081344411d485cd96b574f0a4c
ValkSam <do110473sva@gmail.com>
2015-10-28 21:16:57 +0200

    HW7.1 + public UserMealWithExceed(@JsonProperty("id") Integer id ... для сериализации без конструктора без параметров

--- src/main/java/ru/javawebinar/topjava/to/UserMealWithExceed.java
+++ src/main/java/ru/javawebinar/topjava/to/UserMealWithExceed.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.to;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.LocalDateTime;
 
@@ -22,15 +23,11 @@ public class UserMealWithExceed {
 
     protected final boolean exceed;
 
-    public UserMealWithExceed() {
-        this(null, null, 0, false);
-    }
-
-    public UserMealWithExceed(LocalDateTime dateTime, String description, int calories, boolean exceed) {
-        this(null, dateTime, description, calories, exceed);
-    }
-
-    public UserMealWithExceed(Integer id, LocalDateTime dateTime, String description, int calories, boolean exceed) {
+    public UserMealWithExceed(@JsonProperty("id") Integer id,
+                              @JsonProperty("dateTime") LocalDateTime dateTime,
+                              @JsonProperty("description") String description,
+                              @JsonProperty("calories") int calories,
+                              @JsonProperty("exceed") boolean exceed) {
         this.id = id;
         this.dateTime = dateTime;
         this.description = description;
